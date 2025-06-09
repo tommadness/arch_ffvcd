610b989fe633ecf4d9c1f61aacf2cfb0fad345b2
Tommadness <tom.m.cover@gmail.com>
2025-06-09 06:52:56 -0400

    Fix comment line break

--- items.py
+++ items.py
@@ -177,7 +177,8 @@ def create_world_items(world, trapped_chests_flag = False, chosen_mib_locations 
         if job_name in world.options.jobs_included:
             initial_job_list.append(job_selection_dict[job_name])
     world.random.shuffle(initial_job_list)
-    """ while True:
+    """
+    while True:
         if (initial_job_list[-1] in [JOB_CODE_KNIGHT,JOB_CODE_MONK,JOB_CODE_THIEF,JOB_CODE_DRAGOON,JOB_CODE_NINJA,
                                    JOB_CODE_SAMURAI,JOB_CODE_BERSERKER,JOB_CODE_HUNTER,JOB_CODE_MYSTIC_KNIGHT,
                                    JOB_CODE_TRAINER,JOB_CODE_CHEMIST,JOB_CODE_GEOMANCER,JOB_CODE_BARD,JOB_CODE_DANCER] \
